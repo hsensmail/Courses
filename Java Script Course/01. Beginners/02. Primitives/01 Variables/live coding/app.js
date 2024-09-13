235fffa185cc0504eef9f356a8564d998a326699
Hussein Ismail <154638329+hsensmail@users.noreply.github.com>
2024-09-13 17:15:28 +0300

    Delete Java Script Course/01. Beginners directory

--- Java Script Course/01. Beginners/02. Primitives/01 Variables/live coding/app.js
+++
@@ -1,7 +0,0 @@
-// (reserved keyword): (variable name): (type of value)
-
-let variable; // undefined
-let name = "Hussein Ismail";
-console.log(name);
-const channelName = "Hussein's Academy";
-console.log(channelName);
